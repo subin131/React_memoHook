6482c9160023a46f23ee7ec89b71ce0ef29b09f3
subin131 <65068161+subin131@users.noreply.github.com>
2021-06-12 12:58:22 +0545

    This is memo hook

---
+++ FocusRef.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useRef } from "react";
+
+function FocusRef() {
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+  return (
+    <div>
+      <input ref={inputRef} type="text"></input>
+    </div>
+  );
+}
+
+export default FocusRef;
